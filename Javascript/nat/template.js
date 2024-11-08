b768a6028431c1bfd80bad503112bc63ad8ffd32
Kysetopher <ckyser2@gmail.com>
2024-11-08 10:59:40 -0800

    Menu Headers now Highlight

--- Javascript/nat/template.js
+++ Javascript/nat/template.js
@@ -1,18 +1,32 @@
 
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
+    $(document).ready(function() {
+        $(".navbar .nav-link").on('click', function(event) {
+            if (this.hash !== "") {
+                event.preventDefault();
+                var hash = this.hash;
 
-        if (this.hash !== "") {
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 700, function() {
+                    window.location.hash = hash;
+                });
+            } 
+        });
 
-            event.preventDefault();
+        $(window).on('scroll', function() {
+            var currentScrollPos = $(window).scrollTop() + 100; 
 
-            var hash = this.hash;
+            $('.navbar .nav-link').each(function() {
+                var section = $(this.hash);
+                if (section.length) {
+                    var sectionTop = section.offset().top;
+                    var sectionBottom = sectionTop + section.outerHeight();
 
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 700, function(){
-                window.location.hash = hash;
+                    if (currentScrollPos >= sectionTop && currentScrollPos < sectionBottom) {
+                        $('.navbar .nav-link').removeClass('active');
+                        $(this).addClass('active');
+                    }
+                }
             });
-        } 
+        });
     });
-});
