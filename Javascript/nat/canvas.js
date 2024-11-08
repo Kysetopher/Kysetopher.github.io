4e2882eea9899f46aa0c96fc32b6fc630d62ca21
Kysetopher <ckyser2@gmail.com>
2024-11-08 09:20:31 -0800

    Refractoring and Adjusting the Canvas element

--- Javascript/nat/canvas.js
+++ Javascript/nat/canvas.js
@@ -3,13 +3,13 @@
 //------------------------------- THREE.js Handles -------------------------------------------------------
 var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);	
+var camera = new THREE.PerspectiveCamera(40, window.innerWidth / 500, 0.1, 1000);	
 var renderer = new THREE.WebGLRenderer({ alpha: true });													   
 var light = new THREE.AmbientLight( 0x404040 );											
 
 //---------------------------- DOM Handles -----------------------------------------------------------------
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize( window.innerWidth, 500 );
+document.getElementById('gallery').appendChild(renderer.domElement);
 	
 const display = document.getElementsByTagName("CANVAS")[0];
 const head = document.getElementsByTagName('HEAD')[0];
